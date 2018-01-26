6a2f28e1d2bb2b09889183b30f1f13cdac1cab11
moesama <supervincent.gwx@gmail.com>
2018-01-26 11:29:38 +0800

    add triggers

---
+++ Assets/Scripts/Triggers/TitleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleTrigger : MonoBehaviour {
+    public void OnStart()
+    {
+        SceneManager.LoadScene("Scenes/test");
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+}
